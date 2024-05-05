e9dfd2b85d91864bd38568e4ea265e9c92e298cd
Ankit Rawat <117070701+Re3-1@users.noreply.github.com>
2024-05-05 15:14:08 +0530

    Update account.js

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const { authMiddleware } = require("../middlewares");
 const { Account, Users } = require("../db");
+const { default: mongoose } = require("mongoose");
 const accRouter=express.Router();
 
 accRouter.get("/balance",authMiddleware,async(req,res)=>{
@@ -17,23 +18,33 @@ accRouter.get("/balance",authMiddleware,async(req,res)=>{
 })
 accRouter.post("/transfer",authMiddleware,async(req,res)=>{
         const {to,amount}=req.body
+        const session=await mongoose.startSession();
+        await session.startTransaction();
         const user2=await Account.findOne(
            { userId:to}
-        )
+        ).session(session)
         if(!user2){
+            await session.abortTransaction();
            return  res.status(404).json({
+                
                 msg:"User Doesn't Exist!!"
             })
         }
 
-        const user1=await Account.findOne({userId:req.userID})
+        const user1=await Account.findOne({userId:req.userID}).session(session)
         if(!user1||!user1.balance>=amount){
+            await session.abortTransaction();
            return  res.status(404).json({
+           
                 msg:"InSufficient Balance"
             })
         }
-        await Account.updateOne({userId:req.userID},{$inc:{balance:-amount}})
-        await Account.updateOne({userId:to},{$inc:{balance:+amount}})
+        await Account.updateOne({userId:req.userID},{$inc:{balance:-amount}}).session(session)
+        await Account.updateOne({userId:to},{$inc:{balance:+amount}}).session(session)
+
+
+        await session.commitTransaction()
+       await session.endSession
         res.json({
             msg:"Transfer Succesful!!!"
         })
